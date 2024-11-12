b7701b026d6e65c89463fe2a47aa5fe3f3a29682
David Andersson <lejonmanen@gmail.com>
2024-11-12 15:17:13 +0100

    Lägga till nya element

--- script.js
+++ script.js
@@ -1,14 +1,39 @@
-console.log('Hello world!')
+// console.log('Hello world!')
 
 // document.getElementById
 // . == klasser
 // # == id
 const first = document.querySelector('#first-container')
-console.log(first)
+// console.log(first)
 first.innerText = 'Hello world!'
 
 const button = document.querySelector('#click-button')
 const results = document.querySelector('#click-results')
-button.addEventListener('click', () => {
-	results.innerText = 'Antal klick: ' + 1
+
+let count = 0
+
+button.addEventListener('click', (event) => {
+	count++
+	results.innerText = 'Antal klick: ' + count
+	console.log('Event object: ', event)
+})
+
+
+// Plocka ut element från DOM
+// Skapa event listener - koppla till <button>
+// Skapa nytt <li> element varje gång man klickar
+// Fyll <li> med innehåll - från <input>
+// Lägg till <li> sist i <ul>
+
+const input = document.querySelector('#cv-input')
+const cvButton = document.querySelector('#cv-button')
+const list = document.querySelector('#cv-list')
+
+// console.log('DOM-objekt: ', input, cvButton, list)
+cvButton.addEventListener('click', () => {
+	// console.log('Klickade cv-button')
+	const li = document.createElement('li')  // <li>
+	li.innerText = input.value
+	// console.log('Li: ', li)
+	list.append(li)
 })
